0617e7e7086ff075e5c57e7be1027aa81345c7dc
Ayush Goel <goelayush135@gmail.com>
2025-04-27 10:05:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ import streamlit as st
 import speech_recognition as sr
 import pyttsx3
 import time
+os.environ['DISPLAY'] = ':0' 
 st.set_page_config(page_title="🎙 SAGE Assistant", layout="centered")
 
 # --- CUSTOM CSS ---
